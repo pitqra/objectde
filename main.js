10df68a5489f57ccf211dea1afefb1d0c9d80920
pitqra <86132205+pitqra@users.noreply.github.com>
2022-05-08 10:51:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function one()
+{
+    window.location = "hall.html";
+}
+function two()
+{
+    window.location = "dining.html";
+}
+function three() 
+{
+    window.location = "kitchen.html";
+}
+function four()
+{
+    window.location = "walldrobe.html";
+}
+function five()
+{
+    window.location = "bed.html";
+}
